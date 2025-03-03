d494f8ac862ca6906324ba2684cb042b26128f2a
ElLuchoMan <baluist@correo.udistrital.edu.co>
2025-03-03 15:02:10 -0500

    Added: tests for modal

--- src/app/core/services/modal.service.spec.ts
+++ src/app/core/services/modal.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ModalService } from './modal.service';
 
 describe('ModalService', () => {
@@ -12,5 +11,39 @@ describe('ModalService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have initial modalData as null and isOpen as false', () => {
+    expect(service.getModalData()).toBeNull();
+
+    let isOpenValue: boolean | undefined;
+    service.isOpen$.subscribe(value => {
+      isOpenValue = value;
+    });
+    expect(isOpenValue).toBe(false);
+  });
+
+  it('should update modalData and set isOpen to true when openModal is called', () => {
+    const data = { title: 'Test Modal', content: 'Contenido de prueba' };
+    service.openModal(data);
+
+    expect(service.getModalData()).toEqual(data);
+
+    let isOpenValue: boolean | undefined;
+    service.isOpen$.subscribe(value => {
+      isOpenValue = value;
+    });
+    expect(isOpenValue).toBe(true);
+  });
+
+  it('should set isOpen to false when closeModal is called', () => {
+    service.openModal({ title: 'Test Modal' });
+    service.closeModal();
+
+    let isOpenValue: boolean | undefined;
+    service.isOpen$.subscribe(value => {
+      isOpenValue = value;
+    });
+    expect(isOpenValue).toBe(false);
   });
 });
