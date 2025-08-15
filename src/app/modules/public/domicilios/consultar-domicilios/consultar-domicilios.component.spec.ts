a652f7744673e7f28fe90c4ad1e7d583e17e9953
Bryan Luis <baluisto96@gmail.com>
2025-08-15 15:31:30 -0500

    test countFiltros

--- src/app/modules/public/domicilios/consultar-domicilios/consultar-domicilios.component.spec.ts
+++ src/app/modules/public/domicilios/consultar-domicilios/consultar-domicilios.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { DomicilioService } from '../../../../core/services/domicilio.service';
+import { UserService } from '../../../../core/services/user.service';
+import { TrabajadorService } from '../../../../core/services/trabajador.service';
+import { ModalService } from '../../../../core/services/modal.service';
 import { ConsultarDomicilioComponent } from './consultar-domicilios.component';
 
 describe('ConsultarDomiciliosComponent', () => {
@@ -8,9 +12,14 @@ describe('ConsultarDomiciliosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ConsultarDomicilioComponent]
-    })
-    .compileComponents();
+      imports: [ConsultarDomicilioComponent],
+      providers: [
+        { provide: DomicilioService, useValue: {} },
+        { provide: UserService, useValue: { getUserId: () => {} } },
+        { provide: TrabajadorService, useValue: {} },
+        { provide: ModalService, useValue: {} }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConsultarDomicilioComponent);
     component = fixture.componentInstance;
@@ -19,5 +28,29 @@ describe('ConsultarDomiciliosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return 1 when one filter is active', () => {
+    component.buscarPorDireccion = true;
+    component.buscarPorTelefono = false;
+    component.buscarPorFecha = false;
+
+    expect(component.countFiltros()).toBe(1);
+  });
+
+  it('should return 2 when two filters are active', () => {
+    component.buscarPorDireccion = true;
+    component.buscarPorTelefono = true;
+    component.buscarPorFecha = false;
+
+    expect(component.countFiltros()).toBe(2);
+  });
+
+  it('should return 3 when all filters are active', () => {
+    component.buscarPorDireccion = true;
+    component.buscarPorTelefono = true;
+    component.buscarPorFecha = true;
+
+    expect(component.countFiltros()).toBe(3);
   });
 });
