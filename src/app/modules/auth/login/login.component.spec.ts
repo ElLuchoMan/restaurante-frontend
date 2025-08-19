034179353cd65b2480e1572a5b1ab6a2a2f7f1d3
Bryan Luis <baluisto96@gmail.com>
2025-08-18 20:17:52 -0500

    refactor(login): switch to reactive forms

--- src/app/modules/auth/login/login.component.spec.ts
+++ src/app/modules/auth/login/login.component.spec.ts
@@ -5,10 +5,10 @@ import { ToastrService } from 'ngx-toastr';
 import { UserService } from '../../../core/services/user.service';
 import { LoginComponent } from './login.component';
 import { of, throwError } from 'rxjs';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { mockLogin } from '../../../shared/mocks/login.mock';
-import { LoggingService } from '../../../core/services/logging.service';
+import { LoggingService, LogLevel } from '../../../core/services/logging.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -35,12 +35,11 @@ describe('LoginComponent', () => {
     } as unknown as jest.Mocked<ToastrService>;
 
     const loggingServiceMock = {
-      error: jest.fn(),
       log: jest.fn()
     } as unknown as jest.Mocked<LoggingService>;
 
     await TestBed.configureTestingModule({
-      imports: [LoginComponent, FormsModule, CommonModule],
+      imports: [LoginComponent, ReactiveFormsModule, CommonModule],
       providers: [
         { provide: UserService, useValue: userServiceMock },
         { provide: Router, useValue: routerMock },
@@ -63,53 +62,56 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call login service with correct credentials', () => {
-    userService.login.mockReturnValue(of(mockLoginResponse));
+    it('should call login service with correct credentials', () => {
+      userService.login.mockReturnValue(of(mockLoginResponse));
 
-    component.documento = mockLogin.documento;
-    component.password = mockLogin.password;
-    component.onSubmit();
+      component.loginForm.setValue(mockLogin);
+      component.onSubmit();
 
-    expect(userService.login).toHaveBeenCalledWith(mockLogin);
-    expect(userService.saveToken).toHaveBeenCalledWith('testToken');
-    expect(toastr.success).toHaveBeenCalledWith('Inicio de sesión exitoso', 'Bienvenido Test User');
-  });
+      expect(userService.login).toHaveBeenCalledWith(mockLogin);
+      expect(userService.saveToken).toHaveBeenCalledWith('testToken');
+      expect(toastr.success).toHaveBeenCalledWith('Inicio de sesión exitoso', 'Bienvenido Test User');
+    });
 
-  it('should navigate to admin route when user role is Administrador', () => {
-    userService.login.mockReturnValue(of(mockLoginResponse));
-    userService.getUserRole.mockReturnValue('Administrador');
+    it('should navigate to admin route when user role is Administrador', () => {
+      userService.login.mockReturnValue(of(mockLoginResponse));
+      userService.getUserRole.mockReturnValue('Administrador');
 
-    component.onSubmit();
+      component.loginForm.setValue(mockLogin);
+      component.onSubmit();
 
-    expect(router.navigate).toHaveBeenCalledWith(['/home']);
-  });
+      expect(router.navigate).toHaveBeenCalledWith(['/home']);
+    });
 
-  it('should navigate to client route when user role is cliente', () => {
-    userService.login.mockReturnValue(of(mockLoginResponse));
-    userService.getUserRole.mockReturnValue('Cliente');
+    it('should navigate to client route when user role is cliente', () => {
+      userService.login.mockReturnValue(of(mockLoginResponse));
+      userService.getUserRole.mockReturnValue('Cliente');
 
-    component.onSubmit();
+      component.loginForm.setValue(mockLogin);
+      component.onSubmit();
 
-    expect(router.navigate).toHaveBeenCalledWith(['/home']);
-  });
+      expect(router.navigate).toHaveBeenCalledWith(['/home']);
+    });
 
-  it('should handle login error and show toastr error message from service', () => {
-    const mockError = { message: 'Error de conexión' };
-    userService.login.mockReturnValue(throwError(() => mockError));
+    it('should handle login error and show toastr error message from service', () => {
+      const mockError = { message: 'Error de conexión' };
+      userService.login.mockReturnValue(throwError(() => mockError));
 
-    component.onSubmit();
+      component.loginForm.setValue(mockLogin);
+      component.onSubmit();
 
-    expect(toastr.error).toHaveBeenCalledWith('Error de conexión', 'Error de autenticación');
-    expect(loggingService.error).toHaveBeenCalledWith('err.message:', 'Error de conexión');
-  });
+      expect(toastr.error).toHaveBeenCalledWith('Error de conexión', 'Error de autenticación');
+      expect(loggingService.log).toHaveBeenCalledWith(LogLevel.ERROR, 'Error de inicio de sesión', mockError);
+    });
 
-  it('should handle login error and show generic toastr error message when message is missing', () => {
-    const mockError = {};
-    userService.login.mockReturnValue(throwError(() => mockError));
+    it('should handle login error and show generic toastr error message when message is missing', () => {
+      const mockError = {};
+      userService.login.mockReturnValue(throwError(() => mockError));
 
-    component.onSubmit();
+      component.loginForm.setValue(mockLogin);
+      component.onSubmit();
 
-    expect(toastr.error).toHaveBeenCalledWith('Credenciales incorrectas', 'Error de autenticación');
-    expect(loggingService.error).toHaveBeenCalledWith('No hay propiedad "message" en el error.');
-  });
+      expect(toastr.error).toHaveBeenCalledWith('Credenciales incorrectas', 'Error de autenticación');
+      expect(loggingService.log).toHaveBeenCalledWith(LogLevel.ERROR, 'No hay propiedad "message" en el error.');
+    });
 });
