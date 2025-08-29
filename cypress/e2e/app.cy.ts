ba3a10f6dfce28e7aa67725f158ef1c8dba6c1a3
Bryan Luis <baluisto96@gmail.com>
2025-08-28 19:54:44 -0500

    Revert "Merge branch 'develop' of github.com:ElLuchoMan/restaurante-frontend into develop"
    This reverts commit 5ce06740153f36457ee5bc82b73758e46023baad, reversing
    changes made to 429aeedb06ccc70b6a928f7e19890ea4dc043b45.

--- cypress/e2e/app.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('App', () => {
-  it('should visit the home page', () => {
-    cy.visit('/');
-  });
-});
--- cypress/support/e2e.ts
+++
@@ -1,4 +0,0 @@
-// This file is processed automatically before running tests.
-// Add custom commands or setup here.
-
-export {};
